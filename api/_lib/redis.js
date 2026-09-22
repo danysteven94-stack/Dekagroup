913445a85f6bbe46edf2a943bf27aedb72522952
danysteven94-stack <danysteven94@gmail.com>
2026-09-21 22:34:10 -0400

    Delete api directory

--- api/_lib/redis.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// One shared Upstash Redis client for every API function.
-const { Redis } = require("@upstash/redis");
-
-const redis = Redis.fromEnv();
-
-module.exports = { redis };
